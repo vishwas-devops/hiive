f02dbb1ef39b2c3a99692ca5e6d2333f2b6ae27e
vishwas-devops <35800572+vishwas-devops@users.noreply.github.com>
2026-06-03 11:34:10 -0500

    Update main.tf

--- terraform_infra/main.tf
+++ terraform_infra/main.tf
@@ -1,1 +1,10 @@
 
+module "networking" {
+  source = "./modules/networking"
+
+  name                 = local.name
+  vpc_cidr             = var.vpc_cidr
+  private_subnet_cidrs = var.private_subnet_cidrs
+  availability_zones   = var.availability_zones
+  tags                 = local.tags
+}
