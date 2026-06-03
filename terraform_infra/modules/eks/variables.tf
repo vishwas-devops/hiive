edb6a6d14ad7922cf3d2c483047af4f36357e8a4
vishwas-devops <35800572+vishwas-devops@users.noreply.github.com>
2026-06-03 11:45:51 -0500

    Update variables.tf

--- terraform_infra/modules/eks/variables.tf
+++ terraform_infra/modules/eks/variables.tf
@@ -1,1 +1,45 @@
 
+variable "name" {
+  type        = string
+  description = "Name prefix for EKS resources"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID where EKS will be deployed"
+}
+
+variable "private_subnet_ids" {
+  type        = list(string)
+  description = "Private subnet IDs for EKS worker nodes"
+}
+
+variable "cluster_version" {
+  type        = string
+  description = "Kubernetes version for EKS"
+}
+
+variable "node_instance_types" {
+  type        = list(string)
+  description = "EC2 instance types for EKS managed nodes"
+}
+
+variable "node_desired_size" {
+  type        = number
+  description = "Desired number of worker nodes"
+}
+
+variable "node_min_size" {
+  type        = number
+  description = "Minimum number of worker nodes"
+}
+
+variable "node_max_size" {
+  type        = number
+  description = "Maximum number of worker nodes"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Common tags"
+}
