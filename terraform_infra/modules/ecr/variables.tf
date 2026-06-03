77e1f9d800e1582e3d2400128afa2b165a035b01
vishwas-devops <35800572+vishwas-devops@users.noreply.github.com>
2026-06-03 11:41:54 -0500

    Update variables.tf

--- terraform_infra/modules/ecr/variables.tf
+++ terraform_infra/modules/ecr/variables.tf
@@ -1,1 +1,10 @@
 
+variable "name" {
+  type        = string
+  description = "Name prefix for ECR resources"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Common resource tags"
+}
