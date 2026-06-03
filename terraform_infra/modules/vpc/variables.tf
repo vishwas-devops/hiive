8d5f34e9ab35c6d7d90f6e14b15232fca4b3ec28
vishwas-devops <35800572+vishwas-devops@users.noreply.github.com>
2026-06-03 11:35:37 -0500

    Update variables.tf

--- terraform_infra/modules/vpc/variables.tf
+++ terraform_infra/modules/vpc/variables.tf
@@ -1,21 +1,11 @@
-variable "aws_region" {
+variable "name" {
   type        = string
-  description = "AWS region where resources will be created"
-}
-
-variable "project_name" {
-  type        = string
-  description = "Project name"
-}
-
-variable "environment" {
-  type        = string
-  description = "Environment name"
+  description = "Name prefix for VPC resources"
 }
 
 variable "vpc_cidr" {
   type        = string
-  description = "Main CIDR block for the VPC"
+  description = "CIDR block for the VPC"
 }
 
 variable "private_subnet_cidrs" {
@@ -26,4 +16,9 @@ variable "private_subnet_cidrs" {
 variable "availability_zones" {
   type        = list(string)
   description = "Availability zones for private subnets"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Common resource tags"
 }
